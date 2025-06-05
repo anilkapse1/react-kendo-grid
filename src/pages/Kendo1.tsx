8fbb8500ddaff8d06a933e1187fab104a07d8e7a
anil <anil.k@orioninc.com>
2025-06-05 19:11:57 +0530

    anil-installed first gird example

--- src/pages/Kendo1.tsx
+++ src/pages/Kendo1.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Grid, GridColumn } from "@progress/kendo-react-grid";
+import type { Product } from "../model/IGrid";
 
 const Kendo1 = () => {
+  const products: Product[] = [
+    { id: 1, name: "iPhone", price: 999 },
+    { id: 2, name: "Samsung", price: 899 },
+    { id: 3, name: "OnePlus", price: 699 },
+  ];
   return (
-    <div>
-    </div>
-  )
-}
+    <Grid data={products}>
+      <GridColumn field="id" title="ID" />
+      <GridColumn field="name" title="Product Name" />
+      <GridColumn field="price" title="Price" />
+    </Grid>
+  );
+};
 
-export default Kendo1
+export default Kendo1;
